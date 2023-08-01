0a1cdd2f60f220fdd8b9a0d9c8233f64c1fa999c
jonycorn <jonas.vybiral.jv@gmail.com>
2023-08-01 11:14:08 +0200

    improved links in smaller layouts

--- components/Project.js
+++ components/Project.js
@@ -1,36 +1,10 @@
 import Image from "next/image";
 import { IoGlobeOutline, IoLogoGithub } from "react-icons/io5";
 
-// export default function Project({ name, url, github, text, img }) {
-//   return (
-//     <div className="my-16 lg:my-24 w-screen h-80 min-[450px]:h-[26rem] sm:h-[26rem] md:h-96 flex flex-col items-center justify-center">
-//       <div className="group hover:scale-[1.02] hover:shadow-2xl w-4/5 md:w-3/5 3xl:w-1/2 h-full shadow-xl rounded-[2rem] flex gap-2 flex-col items-center lg:items-start justify-start lg:justify-between cursor-pointer transition-all duration-200 overflow-hidden bg-slate-100 hover:bg-slate-50 relative p-8 md:p-16">
-//         <div className="relative w-full sm:w-auto sm:h-40 md:h-44 lg:h-48 aspect-video">
-//           <Image className="rounded-md" fill src={`/${img}`} alt={name} />
-//         </div>
-//         <div className="flex flex-col items-start justify-between gap-4">
-//           <div>
-//             <h1 className="text-xl font-bold md:text-3xl lg:text-5xl gradient">
-//               {name}
-//             </h1>
-//             <p className="text-xs font-light lg:text-start lg:text-lg">
-//               {text}
-//             </p>
-//           </div>
-//           <div className="flex flex-row items-center justify-center gap-4">
-//             <Button href={url} Icon={IoGlobeOutline} text="Website" />
-//             <Button href={github} Icon={IoLogoGithub} text="GitHub" />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 function Button({ Icon, href, text }) {
   return (
     <a href={href} target="_blank">
-      <button className="flex flex-row items-center justify-center gap-2 p-2 py-2 text-base transition-all duration-200 bg-white border-2 border-white rounded-md shadow-md md:py-2 md:p-4 justify-self-end group-hover:scale-105 group-hover:shadow-lg hover:bg-slate-100 group/button">
+      <button className="flex flex-row items-center justify-center w-full gap-2 p-2 py-2 text-base transition-all duration-200 bg-white border-2 border-white rounded-md shadow-md md:w-auto md:py-2 md:p-4 justify-self-end group-hover:scale-105 group-hover:shadow-lg hover:bg-slate-100 group/button">
         <Icon className="transition-all duration-200 group-hover/button:scale-110" />
         <span className="hidden transition-all duration-200 md:block group-hover/button:font-medium">
           {text}
@@ -41,6 +15,16 @@ function Button({ Icon, href, text }) {
 }
 
 export default function Project({ name, url, github, text, img }) {
+  let links;
+
+  if (github && url) {
+    links = 2;
+  } else if (github || url) {
+    links = 1;
+  } else {
+    links = 0;
+  }
+
   return (
     <div className="transition-all duration-200 flex flex-col md:flex-row justify-center gap-4 w-2/3 md:w-4/5 lg:w-3/5 my-4 lg:my-8 bg-slate-100 hover:bg-slate-50 hover:scale-[1.02] hover:shadow-2xl shadow-xl rounded-[2rem] p-8">
       <div className="relative w-full md:w-1/2 2xl:w-1/3 aspect-video">
@@ -55,7 +39,10 @@ export default function Project({ name, url, github, text, img }) {
             {text}
           </p>
         </div>
-        <div className="flex flex-row items-center justify-center gap-4">
+        <div
+          style={{ gridTemplateColumns: `repeat(${links}, minmax(0, 1fr))` }}
+          className="grid w-full grid-rows-1 gap-4 md:w-auto md:flex md:flex-row md:items-center md:justify-center"
+        >
           {url ? (
             <Button href={url} Icon={IoGlobeOutline} text="Website" />
           ) : null}
